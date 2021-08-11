1ae6adfc105fd7cfd033c871dcb7d28cf0ca0f47
Nga N D HOANG <nguyen.hoang5@live.vu.edu.au>
2021-08-11 17:28:05 +1000

    workshop works

--- Workshop3.R
+++ Workshop3.R
@@ -1,3 +1,7 @@
+library(tidyverse)
 library(tidymodels)
 library(skimr)
 library(janitor)
+
+muffin_cupcake_data_org <- read_csv("https://raw.githubusercontent.com/adashofdata/muffin-cupcake/master/recipes_muffins_cupcakes.csv")
+
